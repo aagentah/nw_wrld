83670d20f68024e2b8d17beeeadee7164df77b48
Jury Giannelli <ix315@me.com>
2026-01-30 23:09:54 +0100

    create modal and dialog atoms

--- src/dashboard/core/modalAtoms.ts
+++ src/dashboard/core/modalAtoms.ts
@@ -1,5 +1,16 @@
 import { atom } from "jotai"
+import { Confirmation } from "../modals/ConfirmationModal"
 
+export const confirmationModalAtom = atom<Confirmation>()
+
+export const workspaceModalAtom = atom(false)
 export const selectSetModalAtom = atom(false)
+export const selectTrackModalAtom = atom(false)
 export const createSetModalAtom = atom(false)
-//  ADD more modals here or one file per modal...
+export const createTrackModalAtom = atom(false)
+export const settingsModalAtom = atom(false)
+export const inputMappingsModalAtom = atom(false)
+export const releaseNotesModalAtom = atom(false)
+export const addModuleModalAtom = atom<"add-to-track" | "manage-modules" | false>(false)
+export const moduleEditorModalAtom = atom(false)
+export const debugOverlayModalAtom = atom(false)
